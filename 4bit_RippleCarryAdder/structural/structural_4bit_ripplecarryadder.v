9f12d61d85ec078bc0b77c64fb534932c8fc8b43
ankita-192007 <ankitamina19@gmail.com>
2026-06-04 10:01:42 +0530

    Add files via upload

---
+++ 4bit_RippleCarryAdder/structural/structural_4bit_ripplecarryadder.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+
+module adder(input A, input B, input Cin, 
+output Sum, output Cout);
+wire w1, w2, w3;
+xor(w1,A,B);
+xor(Sum,w1,Cin);
+and(w2,A,B);
+and(w3,w1,Cin);
+or(Cout,w2,w3);
+endmodule
+
+module structural_4bit_ripplecarryadder(
+input [3:0]A,
+input [3:0]B,
+input Cin,
+output [3:0]Sum,
+output Cout
+);
+
+wire c1,c2,c3;
+
+adder FA0(
+.A(A[0]),
+.B(B[0]),
+.Cin(Cin),
+.Sum(Sum[0]),
+.Cout(c1)
+);
+
+adder FA1(
+.A(A[1]),
+.B(B[1]),
+.Cin(c1),
+.Sum(Sum[1]),
+.Cout(c2)
+);
+
+adder FA2(
+.A(A[2]),
+.B(B[2]),
+.Cin(c2),
+.Sum(Sum[2]),
+.Cout(c3)
+);
+adder FA3(
+.A(A[3]),
+.B(B[3]),
+.Cin(c3),
+.Sum(Sum[3]),
+.Cout(Cout)
+);
+
+endmodule
+
+
